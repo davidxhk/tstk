b5954eba17d7676e9e25cddd9ce1f003f3f3e3a5
David Xie <hk997@live.com>
2025-02-25 05:30:11 +0800

    v1.0.2
    add has function overload

--- src/has.ts
+++ src/has.ts
@@ -5,6 +5,8 @@ import { asProperty, hasProp, isAny, isObject, isReadonly } from "./utils"
 /**
  * Check if a value has K of T
  */
+export function has<const K extends PropertyKey>(value: unknown, prop: K): value is PropertyType<K>
+export function has<const K extends PropertyKey, const T extends Any | Property>(value: unknown, prop: K, type: T): value is PropertyType<K, T>
 export function has<const K extends PropertyKey, const T extends Any | Property>(value: unknown, prop: K, type?: T): value is PropertyType<K, T | undefined> {
   const propType = asProperty(type)
 
